f19a169b901a533a0205e4263a8940e621a704f5
Salavat Khanov <salavat@khanov.com>
2014-01-24 18:27:06 +0600

    Add Info view

---
+++ Time Left/SKInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKInfoViewController.h
+//  Time Left
+//
+//  Created by Salavat Khanov on 1/24/14.
+//  Copyright (c) 2014 Salavat Khanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKInfoViewController : UIViewController
+
+@end
